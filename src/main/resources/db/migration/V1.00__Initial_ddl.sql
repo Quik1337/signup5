ce2ea6f8f5bbfc8c28c6369109e20246f73b039c
Adam <aob.backstrom@gmail.com>
2020-04-27 17:05:39 +0200

    Added 2 new Events

--- src/main/resources/db/migration/V1.00__Initial_ddl.sql
+++ src/main/resources/db/migration/V1.00__Initial_ddl.sql
@@ -36,9 +36,23 @@ insert into user_ (id, first_name, last_name, email) values (5, 'Arzu', 'Oguz', 
 insert into user_ (id, first_name, last_name, email) values (6, 'Jan', 'Grape', 'jan.grape@email.com');
 
 insert into event_ (id, host_id, title, description, date_of_event, time_of_event, location) values (1, 1, 'Daily Standup', 'A meeting where we discuss what we have done and what we are going to do', '2020-07-19', '09:00:00', 'Skype');
+insert into event_ (id, host_id, title, description, date_of_event, time_of_event, location) values (2, 4, 'User Story Presentation', 'We will present our User Story', '2020-04-28', '15:30:00', 'Skype');
+insert into event_ (id, host_id, title, description, date_of_event, time_of_event, location) values (3, 3, 'After Work Gaming', 'We gonna play some 7 Wonders to celebrate', '2020-07-29', '16:00:00', 'Office');
 
 insert into invitation_ (event_id, guest_id) values (1, 1);
 insert into invitation_ (event_id, guest_id) values (1, 2);
 insert into invitation_ (event_id, guest_id) values (1, 3);
 insert into invitation_ (event_id, guest_id) values (1, 4);
 insert into invitation_ (event_id, guest_id) values (1, 5);
+
+insert into invitation_ (event_id, guest_id) values (2, 1);
+insert into invitation_ (event_id, guest_id) values (2, 2);
+insert into invitation_ (event_id, guest_id) values (2, 3);
+insert into invitation_ (event_id, guest_id) values (2, 6);
+
+insert into invitation_ (event_id, guest_id) values (3, 1);
+insert into invitation_ (event_id, guest_id) values (3, 2);
+insert into invitation_ (event_id, guest_id) values (3, 3);
+insert into invitation_ (event_id, guest_id) values (3, 4);
+insert into invitation_ (event_id, guest_id) values (3, 5);
+insert into invitation_ (event_id, guest_id) values (3, 6);
